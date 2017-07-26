480daa74fc4f2bcaff6ef9903395289184d5c8e4
Emily Oppenheimer <opemily@gmail.com>
2017-07-25 22:09:56 -0400

    added battle functionality and started results:

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,4 @@
 var React = require('react');
-var Popular = require('./Popular');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
@@ -7,6 +6,8 @@ var Switch = ReactRouter.Switch
 var Nav = require('./Nav');
 var Home = require ('./Home');
 var Battle = require ('./Battle');
+var Results = require ('./Results');
+var Popular = require('./Popular');
 
 
 
@@ -19,6 +20,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/battle" component={Battle} />
+            <Route path="/battle/results" component={Results}/>
             <Route path="/popular" component={Popular} />
             <Route render={function(){
                 return <p>Not Found</p>
